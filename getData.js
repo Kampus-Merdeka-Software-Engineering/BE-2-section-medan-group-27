d696ab79c0c2b2778c0151556cc857faa981d5c6
Ahmad Ubai Dullah <ahm.idlah773@gmail.com>
2023-12-01 20:11:36 +0700

    init

--- getData.js
+++ getData.js
@@ -8,11 +8,44 @@ async function getData() {
     const jsonData = await fs.readFile(dataPath, 'utf8');
 
     // Parse JSON dan kembalikan sebagai objek
-    return JSON.parse(jsonData);
+    const data = JSON.parse(jsonData);
+
+    // Tampilkan data dalam format HTML
+    const htmlOutput = generateHTML(data);
+
+    return htmlOutput;
   } catch (error) {
     console.error('Gagal membaca data JSON:', error);
     throw error;
   }
+}
+
+function generateHTML(data) {
+  let htmlContent = '<html><head><title>Data Display</title></head><body>';
+
+  // Debugging: Log data untuk memeriksa struktur
+  console.log('Data:', data);
+
+  for (const category in data.categories) {
+    htmlContent += `<h2>${category} News</h2>`;
+    const newsArray = data.categories[category];
+
+    // Debugging: Log newsArray untuk memeriksa struktur
+    console.log('News Array:', newsArray);
+
+    newsArray.forEach(news => {
+      htmlContent += `
+        <div>
+          <h3>${news.title}</h3>
+          <p>${news.content}</p>
+          <p>Published on ${news.publishedAt} by ${news.author}</p>
+        </div>
+      `;
+    });
+  }
+
+  htmlContent += '</body></html>';
+  return htmlContent;
 }
 
 module.exports = getData;
--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-const express = require('express')
-const getData = require('./getData')
-const app = express()
-const port = 3000
-
-
-app.get('/', (req, res) => {
-    res.send('main')
-})
+const express = require('express');
+const getData = require('./getData');
+const app = express();
+const port = 3000;
 
 app.get('/data', async (req, res) => {
   try {
     // Panggil fungsi getData
     const data = await getData();
 
-    // Kirim data sebagai respons
-    res.json(data);
+    // Kirim data sebagai respons HTML
+    res.send(data);
+    // res.json(data)
   } catch (error) {
     res.status(500).send('Internal Server Error');
   }
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
