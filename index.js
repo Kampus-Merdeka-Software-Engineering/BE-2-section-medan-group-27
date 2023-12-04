51fd60edbed8c9b2885236134e9fec8c61586424
Ahmad Ubai Dullah <ahm.idlah773@gmail.com>
2023-12-04 21:25:52 +0700

    init

--- index.js
+++ index.js
@@ -4,18 +4,14 @@ const getData = require('./getData');
 const app = express();
 const port = 3000;
 
-app.get('/data', async (req, res) => {
-  try {
-    // Panggil fungsi getData
-    const data = await getData();
-
-    // Kirim data sebagai respons HTML
-    res.send(JSON.stringify(data));
-    // res.json(data)
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
+app.get('/user', (req, res) => {
+  let user = {
+    name : 'ubai',
+    age : 21
   }
-});
+
+  res.send(JSON.stringify(user))
+})
 
 // Menggunakan controller dari newsController.js untuk endpoint /news
 app.get('/news', getData.getNewsData);
